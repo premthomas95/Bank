2b4225fb8297e51c92107bf65c9c502f542a41be
premthomas95 <thomasprem95@gmail.com>
2020-08-12 20:01:17 -0400

    bank solution with unit test

---
+++ BankTestes/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+
+namespace BankTestes
+{
+    [TestClass]
+    public class BankAccountTests  //The [TestClass] attribute is provided for any class that contains unit test methods that you want to run in Test Explorer.
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Prem Thomas", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
