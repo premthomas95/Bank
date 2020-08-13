8b557ea13db4d4db4761defa3adaed754656ce12
premthomas95 <thomasprem95@gmail.com>
2020-08-12 23:24:41 -0400

    commit - pipeline test

--- BankTestes/UnitTest1.cs
+++ BankTestes/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace BankTestes
             double beginningBalance = 11.99;
             double debitAmount = 4.55;
             double expected = 7.44;
-            BankAccount account = new BankAccount("Prem Thomas P M", beginningBalance);
+            BankAccount account = new BankAccount("Prem Thomas P M - Test", beginningBalance);
 
             // Act
             account.Debit(debitAmount);
